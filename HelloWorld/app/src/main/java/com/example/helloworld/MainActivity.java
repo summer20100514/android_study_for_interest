3d4b023073861022efd10b1eb05c4d138ecbf7c4
summerxu <1032755103@qq.com>
2021-03-21 16:37:17 +0800

    update hello world MainActivity
    Signed-off-by: summerxu <1032755103@qq.com>

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Hello World MainActivity", "onCreate gets executed");
+//        Log.i(TAG, "onCreate: ");
+//        Log.w(TAG, "onCreate: ", );
+//        Log.e(TAG, "onCreate: ", );
+        Log.d(TAG, "onCreate: this log's tag is auto-generated.");
+        Log.d("data", "onCreate: this log can match data log filter");
     }
 }
